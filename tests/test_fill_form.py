19880bab27fb1dd51617c890e1a87120ff1106df
Fedor Chech <fedorchechnevkz@gmail.com>
2023-11-23 13:35:13 +0600

    theerd action

--- tests/test_fill_form.py
+++ tests/test_fill_form.py
@@ -5,7 +5,19 @@ import time
 
 
 @pytest.mark.positive
-def test_fill_form(browser):
+def test_fill_form_1(browser):
+    main_page = TextBox(browser)
+    main_page.go_to_site()
+    main_page.fill_username("Fedor")
+    main_page.fill_email("Fedor@gmail.com")
+    main_page.fill_address("Novgorodskaya 20")
+    main_page.fill_permanent_address("LKJDALKJSDLA")
+    main_page.submit()
+    time.sleep(5)
+
+
+@pytest.mark.positive
+def test_fill_form_2(browser):
     main_page = TextBox(browser)
     main_page.go_to_site()
     main_page.fill_username("Fedor")
